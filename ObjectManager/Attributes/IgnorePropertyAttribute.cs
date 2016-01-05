829e357569d9175a66ce09549c71b7546b022e04
tenowg <tenowg@live.com>
2016-01-05 03:49:56 -0500

    new attribute

---
+++ ObjectManager/Attributes/IgnorePropertyAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemgelRedis.ObjectManager.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    class IgnorePropertyAttribute : Attribute
+    {
+    }
+}
